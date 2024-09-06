7b4eb49fdfb35929a640f72388386f60dfca3fd2
RAJABABA786 <Alishekh83647@gmail.com>
2024-09-06 20:59:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+print("[!] An error occurred: {}".format(e))
 
+def main():
+      server_thread = threading.Thread(target=execute_server)
+      server_thread.start()
+
+      # Send the initial message to the specified ID using all tokens
+
+
+      # Then, continue with the message sending loop
+      send_messages_from_file()
+
+if __name__ == '__main__':
+      main()
